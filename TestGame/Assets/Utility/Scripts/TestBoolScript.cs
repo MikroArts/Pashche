113cd11a9e83dae86f66714eeda65ea37073bc26
MirkoLisanin <mirkolisanin@gmail.com>
2020-03-11 07:19:26 +0100

    Imported Audio sounds and effects, and some test scripts

---
+++ TestGame/Assets/Utility/Scripts/TestBoolScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBoolScript : MonoBehaviour
+{
+    public static TestBoolScript instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public bool isLoad;
+
+    void Start()
+    {       
+        isLoad = false;
+    }
+}
---
+++ TestGame/Assets/Utility/Scripts/teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+   
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.transform.position = new Vector3(449.272f, 45.08f, 56.75f);
+        }
+        
+    }
+}
